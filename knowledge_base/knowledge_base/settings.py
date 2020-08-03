10856226af34218b57da88d38b2f9761562ebe36
Oleg Kamyshov <reg.everywhere@yandex.ru>
2020-08-03 20:16:38 +0300

    add some models

--- knowledge_base/knowledge_base/settings.py
+++ knowledge_base/knowledge_base/settings.py
@@ -51,10 +51,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'knowledge_base.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- knowledge_base/threat_db/models.py
+++ knowledge_base/threat_db/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Treat(models.Model):
+    source_id = models.CharField()
+    name = models.TextField() 
+    description = models.TextField()    
+    sources_of_threat = models.CharField()
+    objects_of_influence = models.CharField()
+    confidentiality_violation = models.BooleanField()
+    integrity_violance = models.BooleanField()
+    accessibility violation = models.BooleanField()
+    pub_date = models.DateField('date of publication')
+    mod_date = models.DateField('date of modification')
+
+class Justification(models.Model):
+    threat = models.ForeignKey(
+        'Treat', on_delete=models.CASCADE, related_name='related_threat'
+    )
+    mod_date = models.DateField('date of modification')
+
+
+
+     
+
+
+
+
+
+
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='post_author'
+    )
+    group = models.ForeignKey(
+        'posts.Group', on_delete=models.CASCADE, blank=True, null=True, 
+        related_name='post_group'
+    )   
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.text
